65f38da061f319457d3e73264c5294f84d871157
Geórgia Pisoni Bender Andrade <georgiapisonibenderandrade@Georgias-MacBook-Air.local>
2025-12-15 11:46:05 +0100

    update

--- G_in.vhd
+++ G_in.vhd
@@ -2,80 +2,125 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
 entity Gate_In is 
-port (
-    clk                      : in std_logic;
-    nrst                     : in std_logic;
-    sensor_A, sensor_B       : in std_logic;
-    barrier                  : out std_logic;
-    Green_Light              : out std_logic;
-    Red_Light                : out std_logic;
-    display                  : out std_logic_vector(6 downto 0)
-);
+    generic (
+        PARKING_CAPACITY : integer := 7
+    );
+    port (
+        clk                      : in std_logic;
+        nrst                     : in std_logic;
+        sensor_A                 : in std_logic;
+        sensor_B                 : in std_logic;
+        can_enter                : in std_logic;  -- Signal from controller indicating spots available
+        barrier                  : out std_logic;
+        inc_car                  : out std_logic; -- Signal to controller to increment car count
+        gate_green_light         : out std_logic; -- Individual gate green light
+        gate_red_light           : out std_logic  -- Individual gate red light
+    );
 end Gate_In;
 
 architecture Behavioural of Gate_In is
-    type state_types is (IDLE, CAR_DETECTED, CAR_ENTERING, FULL, CAR_ENTERED);
-    signal current_state, next_state: state_types;          --states
-    signal barrier, Green_Light, display: state_types;      --outputs
+    type state_types is (IDLE, CAR_DETECTED, CAR_ENTERING, CAR_ENTERED, FULL);
+    signal current_state, next_state: state_types;
+    
+begin
+    -- Sequential process for state register
+    state_register: process(clk, nrst)
+    begin   
+        if nrst = '0' then
+            current_state <= IDLE;
+        elsif rising_edge(clk) then
+            current_state <= next_state;
+        end if;
+    end process;
 
-    -- Car counter
-    signal car_count : integer range 0 to PARKING_CAPACITY := 0;
-
+    -- State transition process
+    state_transition: process(current_state, sensor_A, sensor_B, can_enter)
     begin
-    seq: process(clk, nrst)
-        begin   
-            if(nrst='0') then
-                next_state <= IDLE;
-                current_state <=  IDLE;
-            elsif(rising_edge(clk)) then
-                current_state <= next_state;
-            end if;
-        end process;
-
-    combinatory: process(clk, nrst, sensor_A, sensor_B, barrier, Green_Light, 
-                            Red_Light, display)
-        begin   
-            case state_types is
-                when IDLE =>
-                    if sensor_A ='1' then
-                        next_state<=CAR_DETECTED;
-                end if;
-                when CAR_DETECTED =>
-                    if (sensor_A='0') then
-                        next_state<=IDLE;
-                    elsif (car_count >= PARKING_CAPACITY) then
-                        next_state <= FULL;
-                    elsif(sensor_B='1') then
-                        next_state <= CAR_ENTERING;
-                    elsif car_count >= PARKING_CAPACITY then
-                        next_state <= FULL;
-                    else
+        next_state <= current_state;
+        
+        case current_state is
+            when IDLE =>
+                if sensor_A = '1' then
+                    if can_enter = '1' then
                         next_state <= CAR_DETECTED;
-                end if;
-                when CAR_ENTERING =>
-                    if(sensor_A='0' and sensor_B='1') then
-                        next_state <= CAR_ENTERED;
-                    elsif (sensor_A='1' and sensor_B='0') then
-                        next_state <= CAR_DETECTED;
-                    else
-                        next_state <= CAR_ENTERING;
-                end if;
-                when CAR_ENTERED =>
-                    if sensor_B = '1' then
-                        next_state <= CAR_ENTERED;
-                    else
-                        next_state <= IDLE; 
-                end if;
-                when FULL =>
-                    if sensor_A = '0' or car_count < PARKING_CAPACITY then --A CAR EXITED IN THE WHILE
-                        next_state <= IDLE;
                     else
                         next_state <= FULL;
                     end if;
-                end case;
+                end if;
+                
+            when CAR_DETECTED =>
+                if sensor_A = '0' then
+                    next_state <= IDLE;
+                elsif sensor_B = '1' then
+                    next_state <= CAR_ENTERING;
+                end if;
+                
+            when CAR_ENTERING =>
+                if sensor_A = '0' and sensor_B = '1' then
+                    next_state <= CAR_ENTERED;
+                elsif sensor_A = '1' and sensor_B = '0' then
+                    next_state <= CAR_DETECTED;
+                end if;
+                
+            when CAR_ENTERED =>
+                if sensor_B = '0' then
+                    next_state <= IDLE;
+                end if;
+                
+            when FULL =>
+                if sensor_A = '0' then
+                    next_state <= IDLE;
+                elsif can_enter = '1' then
+                    next_state <= CAR_DETECTED;
+                end if;
+        end case;
+    end process;
+
+    -- Output process
+    output_logic: process(current_state, sensor_A, sensor_B, can_enter)
+    begin
+        -- Default values
+        barrier <= '0';
+        inc_car <= '0';
+        gate_green_light <= '0';
+        gate_red_light <= '0';
+        
+        case current_state is
+            when IDLE =>
+                gate_green_light <= '1';
+                
+            when CAR_DETECTED =>
+                barrier <= '1';
+                gate_green_light <= '1';
+                
+            when CAR_ENTERING =>
+                barrier <= '1';
+                gate_green_light <= '1';
+                
+            when CAR_ENTERED =>
+                barrier <= '1';
+                gate_green_light <= '1';
+                
+                -- Set inc_car when we transition out of this state
+                if sensor_B = '0' then
+                    inc_car <= '1';
+                end if;
+                
+            when FULL =>
+                gate_red_light <= '1';
+                
+                if can_enter = '1' then
+                    gate_green_light <= '1';
+                    gate_red_light <= '0';
+                end if;
+        end case;
+        
+        -- Override lighting based on capacity
+        if current_state = IDLE and can_enter = '0' then
+            gate_green_light <= '0';
+            gate_red_light <= '1';
+        end if;
     end process;
     
-end architecture;
-
+end architecture Behavioural;
--- G_out.vhd
+++ G_out.vhd
@@ -2,3 +2,107 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+entity Gate_Out is 
+    generic (
+        PARKING_CAPACITY : integer := 6
+    );
+    port (
+        clk                      : in std_logic;
+        nrst                     : in std_logic;
+        sensor_A                 : in std_logic;
+        sensor_B                 : in std_logic;
+        payment_done             : in std_logic;
+        can_exit                 : in std_logic;  -- Signal from controller indicating cars available to exit
+        barrier                  : out std_logic;
+        dec_car                  : out std_logic; -- Signal to controller to decrement car count
+        payment_request          : out std_logic; -- Request payment from driver
+        payment_accepted         : out std_logic  -- Signal that payment was accepted
+    );
+end Gate_Out;
+
+architecture Behavioural of Gate_Out is
+    type state_types is (IDLE, WAIT_PAYMENT, PAYMENT_OK, CAR_EXITING, CAR_EXITED);
+    signal current_state, next_state: state_types;
+    
+begin
+    -- Sequential process for state register
+    state_register: process(clk, nrst)
+    begin   
+        if nrst = '0' then
+            current_state <= IDLE;
+        elsif rising_edge(clk) then
+            current_state <= next_state;
+        end if;
+    end process;
+
+    -- State transition process
+    state_transition: process(current_state, sensor_A, sensor_B, payment_done, can_exit)
+    begin
+        next_state <= current_state;
+        
+        case current_state is
+            when IDLE =>
+                if sensor_A = '1' and can_exit = '1' then
+                    next_state <= WAIT_PAYMENT;
+                end if;
+                
+            when WAIT_PAYMENT =>
+                if sensor_A = '0' then
+                    next_state <= IDLE;
+                elsif payment_done = '1' then
+                    next_state <= PAYMENT_OK;
+                end if;
+                
+            when PAYMENT_OK =>
+                if sensor_B = '1' then
+                    next_state <= CAR_EXITING;
+                end if;
+                
+            when CAR_EXITING =>
+                if sensor_A = '0' and sensor_B = '1' then
+                    next_state <= CAR_EXITED;
+                end if;
+                
+            when CAR_EXITED =>
+                if sensor_B = '0' then
+                    next_state <= IDLE;
+                end if;
+        end case;
+    end process;
+
+    -- Output process
+    output_logic: process(current_state, sensor_A, sensor_B, payment_done, can_exit)
+    begin
+        -- Default values
+        barrier <= '0';
+        dec_car <= '0';
+        payment_request <= '0';
+        payment_accepted <= '0';
+        
+        case current_state is
+            when IDLE =>
+                -- No outputs active
+                null;
+                
+            when WAIT_PAYMENT =>
+                payment_request <= '1';
+                
+            when PAYMENT_OK =>
+                payment_accepted <= '1';
+                
+            when CAR_EXITING =>
+                barrier <= '1';
+                payment_accepted <= '1';
+                
+            when CAR_EXITED =>
+                barrier <= '1';
+                payment_accepted <= '1';
+                
+                -- Set dec_car when we transition out of this state
+                if sensor_B = '0' then
+                    dec_car <= '1';
+                end if;
+        end case;
+    end process;
+    
+end architecture Behavioural;
